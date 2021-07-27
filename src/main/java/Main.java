de9f3aca2fa92b55809e50e2f3208a6b37068a9a
Dmitry <x913x913@gmail.com>
2021-07-27 19:08:17 +0500

    Delete Main.java

--- src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-import tests.launcher.TestLauncher;
-
-public class Main {
-    public static void main(String[] args) throws InstantiationException, IllegalAccessException {
-        TestLauncher testLauncher = new TestLauncher("ClassForTesting");
-        testLauncher.launch();
-        testLauncher.showStats();
-    }
-}
